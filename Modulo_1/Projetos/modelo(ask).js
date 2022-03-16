30ea724f9ab89d99449bc2c1f5ac658aeb059910
Malkavianson <alexneto.best@hotmail.com>
2022-03-16 15:52:37 -0300

    Update
    Modelo para usar no p3

---
+++ Modulo_1/Projetos/modelo(ask).js
@@ -0,0 +1,29 @@
+modelo de pergunta:
+
+
+			do{
+			console.log(` \n
+\n		//texto
+				`)
+				console.log(`
+\n	// pergunta
+				`)
+				pergunta = prompt(`SIM[s] ou NÃO[n]`).toLowerCase();
+				if(pergunta != 's' && pergunta != 'n'){
+					prompt(`\nVocê precisa digitar [s] ou [n]\n\npressione ENTER para continuar`)
+				}
+			}while(pergunta != 's' && pergunta != 'n');
+			if(pergunta==='s'){
+				console.clear()
+				console.log(`
+\n	// texto Positivo
+				`)
+				
+			}
+			else{
+				console.clear()
+				console.log(`
+\n	// texto negativo
+				`)			
+			}
+			pressEnter()
