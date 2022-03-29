12eabae289677a5d35aef9b723f9ff03455871e0
Malkavianson <alexneto.best@hotmail.com>
2022-03-28 22:31:04 -0300

    Create M1A16Ex1.js

---
+++ Modulo_1/Exercicios/M1A16Ex1.js
@@ -0,0 +1,46 @@
+//Promisses
+
+function f1(par){
+	
+	return new Promise((resolve, reject) => {
+		console.log(`this is before`);
+		const number = par + 1;
+		
+		resolve(number);
+	});
+}
+
+function f2(par){
+	
+	return new Promise((resolve, reject) => {
+		console.log(`this is after`);
+		const number = par + 1;
+		
+		resolve(number);
+	});
+}
+function f3(par){
+	
+	return new Promise((resolve, reject) => {
+		console.log(par);
+		const number = par + 1;
+		
+		resolve(number);
+	});
+}
+function f4(par){
+	
+	return new Promise((resolve, reject) => {
+		console.log(par);
+		console.log(`this is the end`)
+	});
+}
+
+
+f1(1)
+	.then(f2)
+	.then(f3)
+	.then(f4)
+	.catch((err) => console.log('Error',err))
+
+console.log(`middle`);
