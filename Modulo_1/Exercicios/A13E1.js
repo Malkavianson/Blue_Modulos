734832de8a9861f6de4a7e7a74fecd5d251d9477
Malkavianson <alexneto.best@hotmail.com>
2022-04-06 14:05:12 -0300

    otimização

--- Modulo_1/Exercicios/A13E1.js
+++ Modulo_1/Exercicios/A13E1.js
@@ -9,69 +9,77 @@ const prompt = require('prompt-sync')();
 // weigh
 // height
 
-	let pessoa = {
+	const pessoa = new Object();
 
-		init: function(n,i,p,a) {
-			this.nome 	= n;
-			this.idade	= i;
-			this.peso	= p;
-			this.altura = a;
-			this.vivo	= 1;
-			this.nomei 	= n;
-			this.idadei	= i;
-			this.pesoi	= p;
-			this.alturai= a;
-		},
-		
-		envelhecer: function(){
-			this.idade++;
-		},
-		
-		engordar: function(){
-			this.peso+=100;
-		},
-		
-		emagrecer: function(){
-			this.peso-=100;
-		},
-		
-		crescer: function(){
-			if (this.idade<22){
-				this.altura+=0.5;
-			};
-		},
-		
-		day: function(){
-			let p = Math.round(Math.random() * 3);
-			if(p===1){
-				this.engordar();
-			}else if (p===2){
-				this.emagrecer();
-			}
-		},
-		
-		bio: function(){
-			if(this.idade>121||this.peso>636000||this.altura>272){
-				if(this.idade>121){
-					b = `Você morreu de causa natural, atingiu ${this.idade} anos\n e infelizmente seu corpo não suportou\n		Descanse em paz ${this.nome}`;
-				}else if(this.peso>636000){
-					b = `Você morreu por falha no coração por obesidade aos ${this.peso}\n	Sua família sente que foi um alívio para você!`
-				}else if(this.altura>272){
-					b = `Você morreu em um acidente em uma escada...\n Ao tropeçar em seu próprio cadarço, se desequilibrou e se feriu gravemente no pescoço\n	A natureza não foi em nada generosa com você`
-				}
-				this.vivo = 0;
-			}else{
-				b = `\n		Nome:	${this.nome}\n		Idade:	${this.idade}\n		Peso:	${this.peso/1000}kg\n		Altura:	${this.altura/100}`;
-			}
-			return b;
-		},
+	pessoa.init = function(n,i,p,a) {
+		this.nome 	= n;
+		this.idade	= i;
+		this.peso	= p;
+		this.altura = a;
+		this.vivo	= 1;
+		this.nomei 	= n;
+		this.idadei	= i;
+		this.pesoi	= p;
+		this.alturai= a;
 	};
+		
+	pessoa.envelhecer = function(){
+		this.idade++;
+	},
+	
+	pessoa.engordar = function(){
+		this.peso+=100;
+	},
+	
+	pessoa.emagrecer = function(){
+		this.peso-=100;
+	},
+	
+	pessoa.crescer = function(){
+		if (this.idade<22){
+			this.altura+=0.5;
+		};
+	},
+	
+	pessoa.day = function(){
+		let p = Math.round(Math.random() * 3);
+		if(p===1){
+			this.engordar();
+		}else if (p===2){
+			this.emagrecer();
+		}
+	},
+	
+	pessoa.bio = function(){
+		if(this.idade>121||this.peso>636000||this.altura>272){
+			if(this.idade>121){
+				b = `Você morreu de causa natural, atingiu ${this.idade} anos\n e infelizmente seu corpo não suportou\n		Descanse em paz ${this.nome}`;
+			}else if(this.peso>636000){
+				b = `Você morreu por falha no coração por obesidade aos ${this.peso}\n	Sua família sente que foi um alívio para você!`
+			}else if(this.altura>272){
+				b = `Você morreu em um acidente em uma escada...\n Ao tropeçar em seu próprio cadarço, se desequilibrou e se feriu gravemente no pescoço\n	A natureza não foi em nada generosa com você`
+			}
+			this.vivo = 0;
+		}else{
+			b = `\n		Nome:	${this.nome}\n		Idade:	${this.idade}\n		Peso:	${this.peso/1000}kg\n		Altura:	${this.altura/100}`;
+		}
+		return b;
+	},
 
 
+console.log(`entre [NOME]: `);
+let n = prompt(`		`);
 
+console.log(`entre [IDADE] (em anos) `);
+let i = +prompt(`		`);
 
+console.log(`entre [PESO] (em gramas) `);
+let p = +prompt(`		`);
 
-
+console.log(`entre [ALTURA] (em cm) `);
+let a = +prompt(`		`);
+periodo = 0
+pessoa.init(n,i,p,a)
 
 function y() {
 	let hoje = 1;
@@ -121,20 +129,6 @@ function y() {
 	};
 };
 
-
-console.log(`entre [NOME]: `);
-let n = prompt(`		`);
-
-console.log(`entre [IDADE] (em anos) `);
-let i = +prompt(`		`);
-
-console.log(`entre [PESO] (em gramas) `);
-let p = +prompt(`		`);
-
-console.log(`entre [ALTURA] (em cm) `);
-let a = +prompt(`		`);
-periodo = 0
-pessoa.init(n,i,p,a)
 
 do{
 periodo++
