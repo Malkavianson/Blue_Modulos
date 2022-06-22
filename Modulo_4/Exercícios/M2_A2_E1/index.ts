a40ecd089476304b3331b1cd83e0ca352ff3ccf0
malkavianson <63012624+Malkavianson@users.noreply.github.com>
2022-06-21 22:20:36 -0300

    Final

--- Modulo_4/Exercícios/M2_A2_E1/index.ts
+++ Modulo_4/Exercícios/M2_A2_E1/index.ts
@@ -4,22 +4,40 @@ interface User {
 	occupation: string;
 }
 
-export const users: User[] = [
+interface Admin {
+	name: string;
+	age: number;
+	role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
 	{
-		name: 'Wilker',
+		name: 'Max Mustermann',
 		age: 25,
-		occupation: 'Backend developer',
+		occupation: 'Chimney sweep',
 	},
 	{
-		name: 'Bob',
+		name: 'Jane Doe',
+		age: 32,
+		role: 'Administrator',
+	},
+	{
+		name: 'Kate Müller',
 		age: 23,
-		occupation: 'Product Manager',
+		occupation: 'Astronaut',
+	},
+	{
+		name: 'Bruce Willis',
+		age: 64,
+		role: 'World saver',
 	},
 ];
 
-export function logPerson(user: User) {
-	console.log(` - ${user.name}, ${user.age}`);
+export function logPerson(person: Person) {
+	console.log(` - ${person.name}, ${person.age}`);
 }
 
-console.log('Users:');
-users.forEach(user => logPerson(user));
+console.log('Persons: ');
+persons.forEach(user => logPerson(user));
